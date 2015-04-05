7cbb2e906497fff1cbba15a2a0562621ae4005dd
Shuya Honda <syv2357@gmail.com>
2015-04-05 15:05:44 +0900

    タッチ系のメソッドをOverride

--- TogaPic/ViewController.m
+++ TogaPic/ViewController.m
@@ -66,4 +66,24 @@
 }
 
 
+#pragma mark - Touch Event
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+}
+
+
 @end
